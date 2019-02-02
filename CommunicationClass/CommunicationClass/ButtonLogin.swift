812082ecdc4ecf5ea0e6c8ad19606761a3fa81f1
SarinSwift <sarinyaswift@gmail.com>
2019-02-01 19:49:09 -0800

    pomodoro starter pack

---
+++ CommunicationClass/CommunicationClass/ButtonLogin.swift
@@ -0,0 +1,21 @@
+//
+//  ButtonLogin.swift
+//  CommunicationClass
+//
+//  Created by Sarin Swift on 1/31/19.
+//  Copyright © 2019 Adriana González Martínez. All rights reserved.
+//
+
+import UIKit
+
+class ButtonLogin: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
