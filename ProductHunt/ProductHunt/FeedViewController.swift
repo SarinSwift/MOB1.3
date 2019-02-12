9959e5086d543a395f38cd8024a64dbf7a0cc5d4
SarinSwift <sarinyaswift@gmail.com>
2019-02-12 13:20:57 -0800

    project init

---
+++ ProductHunt/ProductHunt/FeedViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FeedViewController.swift
+//  ProductHunt
+//
+//  Created by Sarin Swift on 2/12/19.
+//  Copyright © 2019 sarinswift. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
+extension FeedViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        return cell
+    }
+    
+    
+    
+    
+}
