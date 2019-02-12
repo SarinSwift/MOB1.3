c9561f80b706f397441b1736060317ace681978d
SarinSwift <sarinyaswift@gmail.com>
2019-02-11 22:22:53 -0800

    json intro: reading data from json file

--- Festival/Festival/FilmEntry.swift
+++ Festival/Festival/FilmEntry.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+struct FilmEntry {
+    var firstActor: String
+    var locations: String
+    var releaseYear: String
+    var title: String
+}
+
+extension FilmEntry {
+    // only an init? can return nil, not init
+    init?(json: [String: Any]) {
+        guard let locations = json["locations"] as? String,
+            let a1 = json["actor_1"] as? String,
+            let year = json["release_year"] as? String,
+            let title = json["title"] as? String
+            else {
+                return nil
+        }
+        self.firstActor = a1
+        self.releaseYear = year
+        self.title = title
+        self.locations = locations
+    }
+}
--- Festival/Festival/ViewController.swift
+++ Festival/Festival/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        let path = Bundle.main.path(forResource: "locations", ofType: ".json")
+        if let path = path {
+            let url = URL(fileURLWithPath: path)
+            print(url)
+            let contents = try? Data(contentsOf: url, options: .alwaysMapped)
+            do {
+                if let data = contents,
+                    let jsonResult = try? JSONSerialization.jsonObject(with: data) as? [[String: Any]] {
+                    print(jsonResult)
+                }
+            }
+            
+        }
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        return cell
+    }
 }
 
