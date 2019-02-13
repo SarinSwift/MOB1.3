02e4ba0d6c1ef61504038772cdaa07ff20023d4a
SarinSwift <sarinyaswift@gmail.com>
2019-02-13 11:51:37 -0800

    Created custom class for Posts

--- ProductHunt/ProductHunt/Cells/PostTableViewCell.swift
+++ ProductHunt/ProductHunt/Cells/PostTableViewCell.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentsCountLabel: UILabel!
+    @IBOutlet weak var votesCountLabel: UILabel!
+    @IBOutlet weak var taglineLabel: UILabel!
+    @IBOutlet weak var previewImageView: UIImageView!
+    
+    // any code in the didSet will execute every time the post variable is set
+    var post: Post? {
+        didSet {
+            guard let post = post else { return }
+            nameLabel.text = post.name
+            taglineLabel.text = post.tagline
+            commentsCountLabel.text = "Comments: \(post.commentsCount)"
+            votesCountLabel.text = "Votes: \(post.votesCount)"
+            updatePreviewImage()
+        }
+    }
+    
+    func updatePreviewImage() {
+        guard let post = post else { return }
+        previewImageView.image = UIImage(named: "placeholder")
+    }
+}
