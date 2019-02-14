bc5ecd5c63c4821adceaae791eb9a91dcc89ca55
SarinSwift <sarinyaswift@gmail.com>
2019-02-13 22:33:57 -0800

    Decodable model

--- ProductHunt/ProductHunt/Models/Post.swift
+++ ProductHunt/ProductHunt/Models/Post.swift
@@ -15,4 +15,36 @@ struct Post {
     let tagline: String
     let votesCount: Int
     let commentsCount: Int
+    var previewImageUrl: URL
+}
+
+struct PostLine: Decodable {
+    var posts: [Post]
+}
+
+extension Post: Decodable {
+    enum PostKeys: String, CodingKey {
+        case id
+        case name
+        case tagline
+        case votesCount = "votes_count"
+        case commentsCount = "comments_count"
+        case previewImageUrl = "screenshot_url"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let postsContainer = try decoder.container(keyedBy: PostKeys.self)
+        id = try postsContainer.decode(Int.self, forKey: .id)
+        name = try postsContainer.decode(String.self, forKey: .name)
+        tagline = try postsContainer.decode(String.self, forKey: .tagline)
+        votesCount = try postsContainer.decode(Int.self, forKey: .votesCount)
+        commentsCount = try postsContainer.decode(Int.self, forKey: .commentsCount)
+        // the screenshot is actually nested in the postsContainer
+        let screenshotUrlContainer = try postsContainer.nestedContainer(keyedBy: PreviewImageUrlKeys.self, forKey: .previewImageUrl)
+        previewImageUrl = try screenshotUrlContainer.decode(URL.self, forKey: .imageUrl)
+    }
+    
+    enum PreviewImageUrlKeys: String, CodingKey {
+        case imageUrl = "850px"
+    }
 }
