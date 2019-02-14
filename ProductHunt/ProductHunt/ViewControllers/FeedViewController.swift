e9dc0b812152a7ae11b4e6c17d5e024bd264441b
SarinSwift <sarinyaswift@gmail.com>
2019-02-14 11:45:59 -0800

    Users can browse products

--- ProductHunt/ProductHunt/ViewControllers/FeedViewController.swift
+++ ProductHunt/ProductHunt/ViewControllers/FeedViewController.swift
@@ -10,34 +10,39 @@ import UIKit
 
 class FeedViewController: UIViewController {
     
-    // Fake data
-    var mockData: [Post] = {
-        var meTube = Post(id: 0, name: "MeTube", tagline: "Stream videos for free!", votesCount: 25, commentsCount: 4)
-        var boogle = Post(id: 1, name: "Boogle", tagline: "Search anything!", votesCount: 1000, commentsCount: 50)
-        var meTunes = Post(id: 2, name: "meTunes", tagline: "Listen to any song!", votesCount: 25000, commentsCount: 590)
-        
-        return [meTube, boogle, meTunes]
-    }()
+    var posts: [Post] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    var networkManager = NetworkManager()
 
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateFeed()
     }
-
+    
+    func updateFeed() {
+        networkManager.getPosts { (result) in
+            self.posts = result
+        }
+    }
 
 }
 
 extension FeedViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mockData.count
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "postCell") as! PostTableViewCell
-        let post = mockData[indexPath.row]
+        let post = posts[indexPath.row]
         cell.post = post
         
         return cell
