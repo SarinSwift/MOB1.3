9af278831b871be5c4d79fe2f1aab02f4de0aa9b
SarinSwift <sarinyaswift@gmail.com>
2019-02-28 19:01:16 -0800

    completed project writing to db, created a Firebase Storage, retreiving data

---
+++ loaner-05-Collection-Views/loaner/Item.swift
@@ -0,0 +1,86 @@
+//
+//  Item.swift
+//  loaner
+//
+//  Created by Erick Sanchez on 8/17/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+import Foundation
+import UIKit.UIImage
+import Contacts.CNContact
+
+struct Item {
+    var itemTitle: String
+    var notes: String = ""
+    // DIDDO: added imageUrl to grab the url from our firebase storage
+    var imageUrl: String?
+    var itemImage: UIImage = UIImage(named: "no item image")!
+    var loanee: Loanee? = nil
+    
+    init(itemTitle: String) {
+        self.itemTitle = itemTitle
+    }
+    
+    init(itemTitle: String, notes: String, itemImage: String, loanee: Loanee?) {
+        self.itemTitle = itemTitle
+        self.notes = notes
+        // DIDDO: changed the imageUrl
+        self.imageUrl = itemImage
+        self.loanee = loanee
+    }
+    
+    mutating func assignLoanee(to contact: CNContact?) {
+        if let contact = contact {
+            
+            //contact image
+            var profileImage = UIImage(named: "no profile image")!
+            if let dataFromContact = contact.imageData,
+                let imageFromContact = UIImage(data: dataFromContact) {
+                profileImage = imageFromContact
+            }
+            
+            //validate contact has at least one number
+            guard let firstPhoneNumber = contact.phoneNumbers.first else {
+                return print("this contact needed to have at least one number from the ContactPickerVc")
+            }
+            
+            //update loanee var
+            let newLoanee = Loanee(
+                name: contact.givenName,
+                profileImage: profileImage,
+                contactNumber: firstPhoneNumber.value.stringValue,
+                contact: contact
+            )
+            
+            loanee = newLoanee
+        } else {
+            loanee = nil
+        }
+    }
+}
+
+struct Loanee {
+    var name: String
+    var profileImage: UIImage = UIImage(named: "no profile image")!
+    var contactNumber: String?
+    var contactInfo: CNContact! = nil
+    
+    init(name: String, profileImage: UIImage, contactNumber: String?) {
+        self.name = name
+        self.profileImage = profileImage
+        self.contactNumber = contactNumber
+    }
+    
+    init(name: String, contactNumber: String?) {
+        self.name = name
+        self.contactNumber = contactNumber
+    }
+    
+    init(name: String, profileImage: UIImage, contactNumber: String?, contact: CNContact) {
+        self.name = name
+        self.profileImage = profileImage
+        self.contactNumber = contactNumber
+        self.contactInfo = contact
+    }
+}
---
+++ loaner-05-Collection-Views/loaner/ViewController.swift
@@ -0,0 +1,171 @@
+//
+//  ViewController.swift
+//  loaner
+//
+//  Created by Erick Sanchez on 8/15/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+    
+    var items: [Item] = []
+    
+    func createNewItem() -> Item {
+        return Item(itemTitle: "Untitled Item")
+    }
+    
+    func add(saved item: Item) {
+        items.insert(item, at: 0)
+        collectionView.insertItems(at: [IndexPath(row: 0, section: 0)])
+    }
+    
+    func markItemAsReturned(at index: Int) {
+        //TODO: archive returned items instead of deleting them
+        deleteItem(at: index)
+        
+    }
+    
+    func deleteItem(at index: Int) {
+        items.remove(at: index)
+        collectionView.deleteItems(at: [IndexPath(row: index, section: 0)])
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            switch identifier {
+            case "show detailed item":
+                guard let detailedItemVc = segue.destination as? ItemDetailedViewController else {
+                    return print("storyboard not set up correctly")
+                }
+                
+                guard
+                    let collectionViewCell = sender as? UICollectionViewCell,
+                    let indexPath = collectionView.indexPath(for: collectionViewCell) else {
+                        return print("'show detailed item' was performed by a non-collection view cell")
+                }
+                
+                let selectedItem = items[indexPath.row]
+                detailedItemVc.item = selectedItem
+            case "show new item":
+                guard let itemEditorVc = segue.destination as? ItemEditorViewController else {
+                    return print("storyboard not set up correctly")
+                }
+                
+                let newItem = createNewItem()
+                itemEditorVc.item = newItem
+            default: break
+            }
+        }
+    }
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    @IBAction func unwindToHome(_ segue: UIStoryboardSegue) {
+        guard let identifier = segue.identifier else {
+            return
+        }
+        
+        switch identifier {
+        case "unwind from back":
+            break
+        case "unwind from trash":
+            guard
+                let selectedIndexPaths = collectionView.indexPathsForSelectedItems,
+                let selectedItemIndexPath = selectedIndexPaths.first else {
+                    return
+            }
+            
+            deleteItem(at: selectedItemIndexPath.row)
+        case "unwind from mark as returned":
+            guard
+                let selectedIndexPaths = collectionView.indexPathsForSelectedItems,
+                let selectedItemIndexPath = selectedIndexPaths.first else {
+                    return
+            }
+            
+            markItemAsReturned(at: selectedItemIndexPath.row)
+        case "unwind from saving new item":
+            guard let itemContactVc = segue.source as? ItemContactInfoViewController else {
+                return print("storyboard not set up correctly")
+            }
+            
+            
+            // DIDDO: uncommenting this part out because the data is saved in firebase database!!
+//            add(saved: itemContactVc.item)
+            
+            
+            
+            
+        default:
+            break
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        //DIDDO: retreive data from the database
+        var ref: DatabaseReference!
+        ref = Database.database().reference().child("items")
+        ref.observe(DataEventType.value) { (snapshot) in
+            
+            print("snapshot issss: \(snapshot)")
+            self.items = []
+            let postDict = snapshot.value as? [String: AnyObject] ?? [:]
+            for rest in postDict {
+                guard let restDict = rest.value as? [String: String] else { continue }
+                guard let name = restDict["name"],
+                    let notes = restDict["notes"],
+                    let url = restDict["imageUrl"] else { return }
+                
+                let person = Loanee(name: "", contactNumber: "")
+                let item = Item(itemTitle: name, notes: notes, itemImage: url, loanee: person)
+                
+                self.items.append(item)
+            }
+            self.collectionView.reloadData()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //set up collection view layout to be half of the screen width and with some padding
+        let flow = UICollectionViewFlowLayout()
+        let screenSize = view.bounds.size
+        let horizontalPadding: CGFloat = 8
+        let verticalPadding: CGFloat = 12
+        flow.itemSize = CGSize(width: screenSize.width / 2 - horizontalPadding * 2, height: screenSize.width / 2 - verticalPadding * 2)
+        flow.sectionInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
+        collectionView.collectionViewLayout = flow
+    }
+}
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item cell", for: indexPath) as! ItemCollectionViewCell
+        
+        let item = items[indexPath.row]
+        cell.configure(item)
+        
+        
+        // DIDDO: Setting each cell's image to the url from firebase storage
+        let url = URL(string: item.imageUrl!)
+        do {
+            let data = try Data(contentsOf: url!)
+            cell.imageView.image = UIImage.init(data: data)
+        } catch {
+            print("error setting image to cell")
+        }
+        
+        return cell
+    }
+}
+
